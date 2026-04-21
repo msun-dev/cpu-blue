a7df1d07a99f90b20175ea197c359e10e358ecff
User <gmchofen@gmail.com>
2026-04-22 03:29:22 +0500

    Updated flags and compiling command

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 # build script for lulz and lalz.
 # nothing fancy but many thing from here will be used in the makefile
 
-flags='-std=c11 -Wall -Wpedantic -Wextra -ggdb -g3 -O0'
+flags='-std=c11 -Wall -Wpedantic -Wextra -ggdb -g3 -O0 -fsanitize=address'
 
 rm -f ./bin/*
 rm -f ./build/*
 gcc $flags -c ./src/cpu.c -o ./build/cpu.o
 gcc $flags -c ./tests/tests.c -o ./build/tests.o
-gcc ./build/cpu.o ./build/tests.o -o ./bin/test
+gcc $flags ./build/cpu.o ./build/tests.o -o ./bin/test
 
